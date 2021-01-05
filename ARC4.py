16be816abff6caa9ebbd79c81a3b98611a409058
cryptogus <60291830+cryptogus@users.noreply.github.com>
2021-01-05 10:58:00 +0900

    스트림사이퍼

---
+++ ARC4.py
@@ -0,0 +1,29 @@
+from Crypto.Cipher import ARC4
+from Crypto.Hash import SHA256 as SHA256
+class myARC4():
+    def __init__(self, keytext):
+        self.key = keytext
+
+    def enc(self, plaintext):
+        arc4 = ARC4.new(self.key)
+        encmsg = arc4.encrypt(plaintext)
+        return encmsg
+
+    def dec(self, ciphertext):
+        arc4 = ARC4.new(self.key)
+        decmsg = arc4.decrypt(ciphertext)
+        return decmsg
+
+def main():
+    keytext = 'samsjang'
+    msg = 'python35'
+
+    myCipher = myARC4(keytext)
+    ciphered = myCipher.enc(msg)
+    deciphered = myCipher.dec(ciphered)
+    print("ORIGINAL:\t%s" %msg)
+    print("CIPHERED:\t%s" %ciphered)
+    print("DECIPHERED:\t%s" %deciphered)
+
+if __name__ =='__main__':
+    main()
